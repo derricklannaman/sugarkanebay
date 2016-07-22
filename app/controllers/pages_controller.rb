df9258d410b1b1191af09ada0c212378a9254b40
Derrick Lannaman <derricklannaman@gmail.com>
2016-07-22 15:26:36 -0500

    add bootstrap, static public facing pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,12 @@ class PagesController < ApplicationController
   def index
   end
 
+  def shop
+  end
+
+  def how_it_works
+  end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root "pages#index"
+  root 'pages#index'
+  get 'about', to: 'pages#about'
+  get 'shop', to: 'pages#shop'
+  get 'how-it-works', to: 'pages#how_it_works', as: 'how'
 end
