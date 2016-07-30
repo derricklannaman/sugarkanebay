d32dfb4161f9ca59a06aaacefe1cf860338ea312
Derrick Lannaman <derricklannaman@gmail.com>
2016-07-30 12:39:08 -0500

    implement cart totaling of selected items

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -3,11 +3,22 @@ class CartController < ApplicationController
   before_action :check_cart_exist, only: :show
 
   def show
-    @orders = current_user.cart.orders if current_user.cart.present?
-  end
+    @orders = current_user.cart.orders.sort if current_user.cart.present?
+    cart_total
+ end
 
   def checkout
-    @orders = current_user.cart.orders
+    @orders = current_user.cart.orders.sort
+    cart_total
+  end
+
+  def cart_total
+    unless current_user.cart.blank?
+      cart_total = current_user.cart.orders.pluck(:total).sum
+      current_user.cart.total = cart_total
+      current_user.cart.save!
+      @cart_total = current_user.cart.total
+    end
   end
 
   def check_cart_exist
