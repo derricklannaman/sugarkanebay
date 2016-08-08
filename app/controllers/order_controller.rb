6c1d84f209aefffc014a9a79a3ed079df238bcce
Derrick Lannaman <derricklannaman@gmail.com>
2016-08-08 13:57:40 -0500

    change from button_to to form_tag for orders, fix janky implementation

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -7,31 +7,34 @@ class OrderController < ApplicationController
   def create
     if current_user.cart.present?
       cart = current_user.cart
-
-      order =  Order.find_or_create_by(order_items: params[:item_name])
       meal = Meal.find(params[:item_id])
+      order =  Order.find_or_create_by(order_items: meal.name)
       if order.order_status == 'inactive'
         # create a new order
         order = Order.create(user_id: current_user.id, meal_id: params[:item_id].to_i,
                       cart_id: cart.id, order_items: params[:item_name],
                       quantity: 1, total: meal.price)
       else
-        # update the active order
+        # update the active order count
         order.user_id = current_user.id if order.user_id.blank?
         order.meal_id = params[:item_id].to_i if order.meal_id.blank?
         order.cart_id = cart.id if order.cart_id.blank?
-        count = order.quantity += 1
+        if params[:quantity].present?
+          count = order.quantity += params[:quantity].to_i
+        else
+          count =  order.quantity += 1
+        end
         order.total = meal.price * count
       end
       order.save!
     else
-      cart = Cart.create!(user_id: current_user.id, owner_name: current_user.firstname, price: '', quantity: '', total: '')
-      current_user.cart = cart
-      order = Order.create( user_id: current_user.id, meal_id: params[:item_id].to_i,
-                    cart_id: cart.id, order_items: params[:item_name], quantity: 1)
+      # cart = Cart.create!(user_id: current_user.id, owner_name: current_user.firstname, price: '', quantity: '', total: '')
+      # current_user.cart = cart
+      # order = Order.create( user_id: current_user.id, meal_id: params[:item_id].to_i,
+      #               cart_id: cart.id, order_items: params[:item_name], quantity: 1)
 
-      current_user.cart.owner_name = current_user.firstname
-      current_user.cart.save!
+      # current_user.cart.owner_name = current_user.firstname
+      # current_user.cart.save!
     end
     flash[:notice] = "#{order.order_items} has been added to your cart"
     redirect_to shop_path
