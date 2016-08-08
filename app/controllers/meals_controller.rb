ad8774a1bcfe280736895c1db2652befc679700f
Derrick Lannaman <derricklannaman@gmail.com>
2016-08-08 13:56:32 -0500

    add a meal count for users

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -4,5 +4,10 @@ class MealsController < ApplicationController
 
   def show
     @meal = Meal.find(params[:id])
+    if current_user.orders.where(meal_id: @meal.id).first.blank?
+      @meal_count = 0
+    else
+      @meal_count = current_user.orders.where(meal_id: @meal.id).first.quantity
+    end
   end
 end
