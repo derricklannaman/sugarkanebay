dc8575a38cfaed21fa4c4b1fbbe8ed5606caeb31
Derrick Lannaman <derricklannaman@gmail.com>
2016-08-11 10:09:33 -0500

    fix meal_count num to only show currently active

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -4,10 +4,12 @@ class MealsController < ApplicationController
 
   def show
     @meal = Meal.find(params[:id])
-    if current_user.orders.where(meal_id: @meal.id).first.blank?
+    if current_user.blank? ||
+      current_user.orders.where(meal_id: @meal.id).first.blank? ||
+      current_user.orders.where(meal_id: @meal.id).where(order_status: 'active').blank?
       @meal_count = 0
     else
-      @meal_count = current_user.orders.where(meal_id: @meal.id).first.quantity
+      @meal_count = current_user.orders.where(meal_id: @meal.id).where(order_status: 'active').first.quantity
     end
   end
 end
