2d7fab96ffe279d7ef89384bc49cb323b937821e
Derrick Lannaman <derricklannaman@gmail.com>
2016-10-01 15:42:50 -0500

    start to address better layout

--- app/assets/javascripts/cart.js
+++ app/assets/javascripts/cart.js
@@ -1,10 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).on('ready page:load', function() {
+  $('span.cart-display').hide();
   $('.checkout__icon').click(showCartContents);
 });
 
 function showCartContents() {
-  console.log('display contents')
+  console.log('ok')
+  $('span.cart-display').show();
 }
 
