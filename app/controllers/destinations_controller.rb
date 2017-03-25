1d6ed99fbba65549cf30971c110504a2df9d0be8
Derrick Lannaman <derricklannaman@gmail.com>
2017-03-25 18:22:16 -0500

    rubocop offenses down to 399 lol

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,5 +1,5 @@
+# TODO: to be implemented at a later date
 class DestinationsController < ApplicationController
-
   # before_action :find_destination, except: :index
   # before_action :immersive_layout, except: [:index]
 
@@ -13,35 +13,28 @@ class DestinationsController < ApplicationController
   #   binding.pry
   # end
 
-  def music_immersive
-  end
+  def music_immersive; end
 
-  def points_of_interest_immersive
-  end
+  def points_of_interest_immersive; end
 
-  def culture_immersive
-  end
+  def culture_immersive; end
 
-  def foods_immersive
-  end
+  def foods_immersive; end
 
-  def art_immersive
-  end
+  def art_immersive; end
 
-  def political_immersive
-  end
+  def political_immersive; end
 
-  def geography_immersive
-  end
+  def geography_immersive; end
 
   private
 
-    def immersive_layout
-      @discovery_contents = @destination.discovery_contents
-      render layout: "immersive"
-    end
+  def immersive_layout
+    @discovery_contents = @destination.discovery_contents
+    render layout: 'immersive'
+  end
 
-    def find_destination
-      @destination = Destination.find(params[:id])
-    end
+  def find_destination
+    @destination = Destination.find(params[:id])
+  end
 end
